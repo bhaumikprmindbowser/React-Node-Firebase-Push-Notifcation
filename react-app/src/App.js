c2b13bec907697cedde4e159206385ab0a57cd2b
bhaumikprmindbowser <bhaumik.parmar@mindbowser.com>
2024-05-20 16:10:45 +0530

    backend message

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,19 +3,36 @@ import "./App.css"
 import NotificationComp from "./component/Notification"
 import AuthProvider from "./provider/AuthProvider"
 import Routes from "./routes"
+import {config} from "./config"
 
 function App() {
-  useEffect(() => {
+  const registerServiceWorker = async () => {
+    let scriptURL = "./firebase-messaging-sw.js"
+    scriptURL += `?apiKey=${config.firebaseConfig.apiKey}`
+    scriptURL += `&authDomain=${config.firebaseConfig.authDomain}`
+    scriptURL += `&projectId=${config.firebaseConfig.projectId}`
+    scriptURL += `&databaseURL=${config.firebaseConfig.databaseURL}`
+    scriptURL += `&storageBucket=${config.firebaseConfig.storageBucket}`
+    scriptURL += `&messagingSenderId=${config.firebaseConfig.messagingSenderId}`
+    scriptURL += `&appId=${config.firebaseConfig.appId}`
+
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("./firebase-messaging-sw.js")
-        .then((registration) => {
-          console.log("Registration successful, scope is:", registration.scope)
-        })
-        .catch((err) => {
-          console.log("Service worker registration failed, error:", err)
-        })
+      try {
+        const registration = await navigator.serviceWorker.register(scriptURL)
+        if (registration.installing) {
+          console.log("Service worker installing")
+        } else if (registration.waiting) {
+          console.log("Service worker installed")
+        } else if (registration.active) {
+          console.log("Service worker active")
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`)
+      }
     }
+  }
+  useEffect(() => {
+    registerServiceWorker()
   }, [])
 
   return (
